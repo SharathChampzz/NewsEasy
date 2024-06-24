a930313cca08d4067e8d6299cde1fd9e3bf83238
Sharath Kumar HK <sharathkumar.mskj.1999@gmail.com>
2024-06-24 10:37:10 +0530

    Rename project from djangos to NewsEasy - Status Working Fine

--- NewsEasy/wsgi.py
+++ NewsEasy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangos project.
+WSGI config for NewsEasy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsEasy.settings')
 
 application = get_wsgi_application()
